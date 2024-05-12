0cdcfb03c7eae314456e7ebb63c3527eb8b3a9bd
Advay Chandorkar <advay1612@icloud.com>
2024-05-12 00:18:57 -0400

    Update P2 - Candy Cane Catastrophe.py

--- P2 - Candy Cane Catastrophe/P2 - Candy Cane Catastrophe.py
+++ P2 - Candy Cane Catastrophe/P2 - Candy Cane Catastrophe.py
@@ -1,11 +1,5 @@
-def min_candy_canes(flavours, counts):
-    min_count = min(counts)
-    total_candy_canes = sum(counts)
-    result = total_candy_canes - min_count + 1
-    return result
-
 n = int(input())
 counts = list(map(int, input().split()))
-
-result = min_candy_canes(n, counts)
-print(result)
+min_count = min(counts)
+total_candy_canes = sum(counts)
+print(total_candy_canes - min_count + 1)
